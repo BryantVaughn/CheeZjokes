d39da22f9d96616eae2c428003fe913243402913
Bryant Vaughn <bf_vaughn@hotmail.com>
2020-03-18 11:08:45 -0600

    styles for JokeList component and include uuid dependency

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
+import Joke from './Joke';
+import axios from 'axios';
+import './JokeList.css';
+
+const API_URL = "https://icanhazdadjoke.com/";
 
 class JokeList extends Component {
+  static defaultProps = {
+    numJokesToGet: 10
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokes: []
+    };
+  }
+
+  async componentDidMount() {
+    let jokes = [];
+    while(jokes.length < this.props.numJokesToGet) {
+      let res = await axios.get(API_URL, {
+        headers: { Accept: "application/json" }
+      });
+      jokes.push({ text: res.data.joke, votes: 0 });
+    }
+
+    this.setState({ jokes: jokes });
+  }
+
   render() {
     return (
-      <div>
-        <h1>List-O-Jokes</h1>
+      <div className="JokeList">
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title">
+            <span>Dad</span> Jokes
+          </h1>
+          <img
+            src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+            alt="Cry Laughing Emoji"
+          />
+          <button className="JokeList-getmore">New Jokes</button>
+        </div>
+
+        <div className="JokeList-jokes">
+          {this.state.jokes.map(j => (
+            <Joke votes={ j.votes } text={ j.text } />
+          ))}
+        </div>
       </div>
-    )
+    );
   }
 }
 
