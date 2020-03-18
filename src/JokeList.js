917d9686ab48b163ab7d7d68509742f94c118e85
Bryant Vaughn <bf_vaughn@hotmail.com>
2020-03-18 11:55:50 -0600

    fix bug which lost up/down votes on reload

--- src/JokeList.js
+++ src/JokeList.js
@@ -16,9 +16,10 @@ class JokeList extends Component {
     this.state = {
       jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  async componentDidMount() {
+  componentDidMount() {
     if(this.state.jokes.length === 0) this.getJokes();
   }
 
@@ -31,8 +32,12 @@ class JokeList extends Component {
       jokes.push({ id: uuidv4(), text: res.data.joke, votes: 0 });
     }
 
-    this.setState({ jokes: jokes });
-    window.localStorage.setItem("jokes", JSON.stringify(jokes));
+    this.setState(st => ({
+      jokes: [...st.jokes, ...jokes]
+    }),
+    () =>
+      window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+    );
   }
 
   handleVote(id, delta) {
@@ -40,7 +45,13 @@ class JokeList extends Component {
       jokes: st.jokes.map(j =>
         j.id === id ? {...j, votes: j.votes + delta } : j
       )
-    }));
+    }),
+    () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+    );
+  }
+
+  handleClick() {
+    this.getJokes()
   }
 
   render() {
@@ -54,7 +65,7 @@ class JokeList extends Component {
             src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
             alt="Cry Laughing Emoji"
           />
-          <button className="JokeList-getmore">New Jokes</button>
+          <button className="JokeList-getmore" onClick={ this.handleClick }>New Jokes</button>
         </div>
 
         <div className="JokeList-jokes">
